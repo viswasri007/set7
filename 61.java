6305f19653d7c0e14365dfc802fa01e1d4522c1d
Viswa Sunny <viswasunny007@gmail.com>
2018-08-17 10:01:59 +0530

    Create 61.java

---
+++ 61.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class Guvi
+{
+     public static void main(String aa[])
+     {
+     String d;
+     int n;
+     Scanner s=new Scanner(System.in);
+     d=s.next();
+     n=s.nextInt();
+     char a[]=d.toCharArray();
+     for(int i=0;i<n;i++)
+     {
+        System.out.print(a[i]);
+     }
+}
+}
