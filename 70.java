190175b55c890f1a356b91e51e48b847b72ac810
Viswa Sunny <viswasunny007@gmail.com>
2018-08-17 11:29:42 +0530

    Create 70.java

---
+++ 70.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();boolean f=true;
+		for(int i=2;f=true;i=i*2)
+		{
+			if(i>n)
+			{
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+}
