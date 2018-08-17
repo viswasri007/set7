d2964dff3f0ed4b3b4403ff4471a0df08bd05b4f
Viswa Sunny <viswasunny007@gmail.com>
2018-08-17 10:53:40 +0530

    Create 65.java

---
+++ 65.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.println(s);
+	}
+}
